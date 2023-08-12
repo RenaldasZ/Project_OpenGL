0abd41879cce027d6897461b53812c93f3cb29d2
AndriusG <agedvilasplay@gmail.com>
2023-08-12 17:10:04 +0300

    Save Project

---
+++ OpenGL/Lesson005/Python Graphics/test2.py
@@ -0,0 +1,45 @@
+from core.base import Base
+from core.openGLUtils import OpenGLUtils
+from OpenGL import GL
+
+# render a single point
+class Test(Base):
+
+    def initialize(self):
+        print("Initializing program...")
+
+        ### create GPU program ###
+
+        # vertex shader code
+        vsCode = """
+        void main()
+        {
+            gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+        }
+        """
+
+        # fragment shader code
+        fsCode = """
+        out vec4 fragColor;
+        void main()
+        {
+            gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+        }
+        """
+
+        # send code to GPU, compile, store program reference
+        self.programRef = OpenGLUtils.initializeProgram(vsCode, fsCode)
+
+        # render settings (optional)
+        GL.glPointSize(16)
+
+    def update(self):
+
+        # select program to use when rendering
+        GL.glUseProgram(self.programRef)
+
+        # renders geometric object(s) using program
+        GL.glDrawArrays(GL.GL_POINTS, 0, 1)
+
+# create instance of class and run it
+Test().run()
---
+++ OpenGL/Lesson005/README.txt
@@ -0,0 +1,4 @@
+Learn how to create vertex array objects to manage the associations between vertex buffer data and attribute ("in") variables in a vertex shader. 
+Then, create an application that uses a vertex array object and the Attribute class to draw a hexagon and illustrates different draw modes 
+(points, lines, line loop, triangles, triangle fan). 
+Create a second application that uses multiple vertex array objects to draw multiple shapes at once.
